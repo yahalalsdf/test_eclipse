b65a3e533b85b6fa5d7aeedfd183487f0e905256
hyunwoo Yang <gus5162.naver.com>
2019-04-23 12:08:11 +0900

    hotfix branch change a code

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -5,6 +5,7 @@ public class test_class {
 	public static void main(String[] args) {
 		System.out.println("okay");
 		System.out.println("okay thankyou");
+		System.out.println("i'm the hotfix");
 
 	}
 
